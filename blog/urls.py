c1bcb349695ba9bb18af7312478107b2b1fe16a7
huiyeetan <huiyeetan@hotmail.com>
2016-06-25 17:26:00 +0800

    Edited urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,16 @@ from django.conf.urls import url
 
 from . import views
 
+
+from django.conf.urls import include, url
+import django.contrib.auth.views
+
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = [
+	url(r'^admin/', include(admin.site.urls)),
+	
 	url(r'^$', views.post_list, name='post_list'),
 
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
@@ -18,7 +27,7 @@ urlpatterns = [
 	url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 
 	url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
-	
+
 	url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
 	
 	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
